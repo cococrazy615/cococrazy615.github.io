a0b1b2f743a6b4265a4d934945cc3c2f8a3b40c0
cococrazy615 <57266397+cococrazy615@users.noreply.github.com>
2019-11-06 20:34:36 -0600

    Add files via upload

---
+++ madlibs.js
@@ -0,0 +1,14 @@
+
+
+var adjective = prompt('Type in an adjective')
+
+var verb = prompt("Type in a noun");
+
+var noun = prompt("Type a verb (present tense)");
+
+alert("Creating your MadLib....")
+
+var sentence = "<h2> On one fine day, a " + adjective;
+sentence += ' chicken wanted to cross a road. All of a sudden a " + noun;
+sentence += ' stopped the chicken in the middle of the road. The " + noun;
+sentence += ' proceeded to ' + verb + ' until he got hit by a car.';
